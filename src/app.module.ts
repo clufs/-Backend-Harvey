fbd8b1628e30b77c741ce5854b1aa7799e50100d
Mauro Nahuel Bautista <mnahuelbautista@gmail.com>
2023-11-21 03:29:31 -0300

    agregando .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,15 +12,17 @@ import { EmployeLoginModule } from './employe-login/employe-login.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      ssl:
-        process.env.STATE === 'prod'
-          ? { rejectUnauthorized: false, sslmode: 'require' }
-          : (false as any),
+      ssl: true,
+      // ssl:
+      //   process.env.STATE === 'prod'
+      //     ? { rejectUnauthorized: false, sslmode: 'require' }
+      //     : (false as any),
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
+
       autoLoadEntities: true,
       synchronize: true,
     }),
